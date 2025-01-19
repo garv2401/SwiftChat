c18f576420e11d9cb92ff95a983b475fb99d8789
Garvit <garvitkumar504@gmail.com>
2025-01-19 20:59:14 +0530

    Updated apiConfig

--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -2,8 +2,8 @@ let baseURL;
 let socketURL;
 
 if(import.meta.env.VITE_NODE_ENV==="production"){
-    baseURL="deployed-url";
-    socketURL="deployed-url";
+    baseURL="https://swiftchat-oasr.onrender.com";
+    socketURL="ws://swiftchat-oasr.onrender.com";
 }else{
     baseURL="http://localhost:4000";
     socketURL="ws://localhost:4000";
