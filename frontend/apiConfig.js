75174e4a3dcbcd0aed5ef5bdae22e179e3d1c7c9
Garvit <garvitkumar504@gmail.com>
2025-01-19 21:03:31 +0530

    updated apiConfig

--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -1,12 +1,15 @@
 let baseURL;
 let socketURL;
 
-if(import.meta.env.VITE_NODE_ENV==="production"){
-    baseURL="https://swiftchat-oasr.onrender.com";
-    socketURL="ws://swiftchat-oasr.onrender.com";
-}else{
-    baseURL="http://localhost:4000";
-    socketURL="ws://localhost:4000";
-}
+// if(import.meta.env.VITE_NODE_ENV==="production"){
+//     baseURL="https://swiftchat-oasr.onrender.com";
+//     socketURL="ws://swiftchat-oasr.onrender.com";
+// }else{
+//     baseURL="http://localhost:4000";
+//     socketURL="ws://localhost:4000";
+// }
+
+baseURL="https://swiftchat-oasr.onrender.com";
+socketURL="ws://swiftchat-oasr.onrender.com";
 
 export {baseURL,socketURL};
