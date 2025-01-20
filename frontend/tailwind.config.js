79394cecebc2e8aead4dc228a1a63dabd8fd0778
Garvit <garvitkumar504@gmail.com>
2025-01-20 20:01:07 +0530

    Updated tailwind.config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 export default {
-  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        dark:"#131313",
-        background:"#202329",
-        primary:"#3f4654",
-        primarySecond:"#6B8AFD",
-        secondary:"#30E0A1",
-        tertiary:"#EB7050"
-      }
+      colors: {
+        dark: "#131313",
+        background: "#202329",
+        primary: "#3f4654",
+        primarySecond: "#6B8AFD",
+        secondary: "#30E0A1",
+        tertiary: "#EB7050",
+      },
     },
   },
   plugins: [
-    require('tailwind-scrollbar'), 
+    tailwindScrollbar,
   ],
 };
-
