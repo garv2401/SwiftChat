f682c0f7b32280386e2d7d79259a2b951b46943d
Garvit <garvitkumar504@gmail.com>
2025-01-22 13:43:01 +0530

    Update loginControl

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -31,6 +31,7 @@ const loginController=async(req,res)=>{
             httpOnly:false,
             sameSite:"none",
             secure:true,
+            signed:true,
             expires:new Date(Date.now()+7*24*60*60*1000),
            })
            .send({message:"Login Sucessful",status:200});
