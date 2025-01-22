edd28f65d1875690c2a0295364e34d5518959dd8
Garvit <garvitkumar504@gmail.com>
2025-01-22 22:50:24 +0530

    Updated removeCookie

--- server/controllers/removeCookieController.js
+++ server/controllers/removeCookieController.js
@@ -1,16 +1,20 @@
-
-
-const removeCookieController=async(req,res)=>{
-
-    const token=req.cookies["authToken"];
-
-    if(token){
-        res.clearCookie("authToken", { path: "/" });
-        return res.status(200).send({ message: "Cookie removed" });
-    }else{
-        return res.status(401).send({ message: "authToken cookie doesnt exists" });
+const removeCookieController = async (req, res) => {
+    const token = req.cookies["authToken"];
+  
+    if (token) {
+      // Clear the cookie with matching options
+      res.clearCookie("authToken", {
+        httpOnly: false, // Matches original setting
+        sameSite: "none", // Matches original setting
+        secure: true, // Matches original setting
+        path: "/", // Default path
+      });
+  
+      return res.status(200).send({ message: "Cookie removed" });
+    } else {
+      return res.status(401).send({ message: "authToken cookie doesn't exist" });
     }
-
-}
-
-module.exports=removeCookieController;
+  };
+  
+  module.exports = removeCookieController;
+  
