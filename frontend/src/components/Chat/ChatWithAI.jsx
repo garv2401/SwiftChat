26404e47049920baa68ea2f17cdccbcaa4264c28
GARVIT <118100178+garv2401@users.noreply.github.com>
2025-11-09 12:27:36 +0530

    Update AI model from gemini-1.5 to gemini-2.0

--- frontend/src/components/Chat/ChatWithAI.jsx
+++ frontend/src/components/Chat/ChatWithAI.jsx
@@ -6,7 +6,7 @@ const ChatWithAI = (userDetails) => {
   const genAI = new GoogleGenerativeAI(
     "AIzaSyBHrVfuLN-M4nLiLlwLcvVKum4v5Jp-28Q"
   );
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
   const [newPrompt, setNewPrompt] = useState(null);
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState(null);
